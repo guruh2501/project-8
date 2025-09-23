1ee1ac5ecaf959594a1c7e00a57ff76b05c871cd
Guruh <Guruh.2501@gmail.com>
2025-09-23 19:52:20 +0700

    berhasil Tahap2 membuat tampilan web produk

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/db");
 const produkRoutes = require("./routes/produkRoutes");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 
-// Routing utama
+// Setting EJS
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Routing API
 app.use("/api/produk", produkRoutes);
 
-// Cek koneksi + Sync DB
+// Routing UI
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("http://localhost:3000/api/produk");
+    const produk = response.data;
+    res.render("index", { produk });
+  } catch (err) {
+    res.render("index", { produk: [] });
+  }
+});
+
+// Sync DB
 sequelize.sync().then(() => {
   console.log("Database connected & synced!");
   app.listen(3000, () => console.log("Server running at http://localhost:3000"));
